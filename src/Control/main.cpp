ef26b0847f6a8c320f24629a1021100ef1aa5099
Alex Gurney <thaeds@gmail.com>
2016-04-17 02:33:55 +0000

    Motors 1 and 2 move in series, need to fix tilt.yaml to support multiple motors simultaeously  OMGAMAZING

--- src/Control/main.cpp
+++ src/Control/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "control.h"
+#include <ros/ros.h>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    cerr << "Hello World" << endl;
-    return 0;
+    ros::init(argc, argv, "control");
+    cerr << "Control Node Initialized" << endl;
+    ros::NodeHandle control_nh;
+    ros::spin();
 }
